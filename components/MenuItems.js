4b286702d83a7d2bd07ab1b2186c18fb0cd27084
John Lundy <johnclundyy@gmail.com>
2024-06-17 12:51:52 -0600

    sectionlist2

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -6,39 +6,50 @@ const menuItemsToDisplay = [
   {
     title: "Appetizers",
     data: [
-      "Hummus",
-      "Moutabal",
-      "Falafel",
-      "Marinated Olives",
-      "Kofta",
-      "Eggplant Salad",
+      { name: "Hummus", price: "$5.00" },
+      { name: "Moutabal", price: "$5.00" },
+      { name: "Falafel", price: "$7.50" },
+      { name: "Marinated Olives", price: "$5.00" },
+      { name: "Kofta", price: "$5.00" },
+      { name: "Eggplant Salad", price: "$8.50" },
     ],
   },
   {
     title: "Main Dishes",
-    data: ["Lentil Burger", "Smoked Salmon", "Kofta Burger", "Turkish Kebab"],
+    data: [
+      { name: "Lentil Burger", price: "$10.00" },
+      { name: "Smoked Salmon", price: "$14.00" },
+      { name: "Kofta Burger", price: "$11.00" },
+      { name: "Turkish Kebab", price: "$15.50" },
+    ],
   },
   {
     title: "Sides",
     data: [
-      "Fries",
-      "Buttered Rice",
-      "Bread Sticks",
-      "Pita Pocket",
-      "Lentil Soup",
-      "Greek Salad",
-      "Rice Pilaf",
+      { name: "Fries", price: "$3.00", id: "11K" },
+      { name: "Buttered Rice", price: "$3.00" },
+      { name: "Bread Sticks", price: "$3.00" },
+      { name: "Pita Pocket", price: "$3.00" },
+      { name: "Lentil Soup", price: "$3.75" },
+      { name: "Greek Salad", price: "$6.00" },
+      { name: "Rice Pilaf", price: "$4.00" },
     ],
   },
   {
     title: "Desserts",
-    data: ["Baklava", "Tartufo", "Tiramisu", "Panna Cotta"],
+    data: [
+      { name: "Baklava", price: "$3.00" },
+      { name: "Tartufo", price: "$3.00" },
+      { name: "Tiramisu", price: "$5.00" },
+      { name: "Panna Cotta", price: "$5.00" },
+    ],
   },
 ];
 
-const Item = ({ name }) => (
+const Item = ({ name, price }) => (
   <View style={menuStyles.innerContainer}>
     <Text style={menuStyles.itemText}>{name}</Text>
+    <Text style={menuStyles.itemText}>{price}</Text>
   </View>
 );
 
@@ -51,7 +62,7 @@ const Footer = () => (
 );
 
 const MenuItems = () => {
-  const renderItem = ({ item }) => <Item name={item} />;
+  const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
 
   const renderSectionHeader = ({ section: { title } }) => (
     <Text style={menuStyles.sectionHeader}>{title} </Text>
@@ -71,34 +82,26 @@ const MenuItems = () => {
   );
 };
 
-// Add styles to the component
 const menuStyles = StyleSheet.create({
   container: {
-    flex: 0.95,
+    flex: 1,
   },
   innerContainer: {
     paddingHorizontal: 40,
     paddingVertical: 20,
-    backgroundColor: "#333333",
-  },
-  sectionHeader: {
-    backgroundColor: "#fbdabb",
-    color: "#333333",
-    fontSize: 34,
-    flexWrap: "wrap",
-    textAlign: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   itemText: {
     color: "#F4CE14",
-    fontSize: 32,
-  },
-  separator: {
-    borderBottomWidth: 1,
-    borderColor: "#EDEFEE",
-  },
-  footerText: {
-    color: "#EDEFEE",
     fontSize: 20,
+  },
+  headerStyle: {
+    backgroundColor: "#F4CE14",
+  },
+  sectionHeader: {
+    color: "white",
+    fontSize: 26,
     flexWrap: "wrap",
     textAlign: "center",
   },
